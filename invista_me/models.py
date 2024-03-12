8390d00f1b7e9c498cd32d98b678253735c5b6d2
saulosgil <saulosgil@hotmail.com>
2024-03-12 07:06:32 -0300

    Models - Uma maneira mais organizada de passar dados

--- invista_me/models.py
+++ invista_me/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+'''
+* Investimento
+* valor
+* pago
+* data
+'''
+
+class Investimento(models.Model):
+  investimento = models.TextField(max_length=255)
+  valor = models.FloatField()
+  pago = models.BooleanField(default=False)
+  data = models.DateField(default=datetime.now)
+
