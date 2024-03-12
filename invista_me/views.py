360d68007de55f42635e16e835869b03c178d701
saulosgil <saulosgil@hotmail.com>
2024-03-12 09:03:42 -0300

    Listando os investimentos

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Investimento
 
 # Create your views here.
-def pagina_inicial(request):
-  return HttpResponse('Printo para investir')
-
 def novo_investimento(request):
   return render(request, 'investimentos/novo_investimento.html')
 
-def investimento_registrado(request):
-  investimento = {
-    'tipo_investimento': request.POST.get('TipoInvestimento')
+def investimentos(request):
+  dados = {
+    'dados': Investimento.objects.all()
   }
-  return render(request, 'investimentos/investimento_registrado.html', investimento)
+  return render(request, 'investimentos/investimentos.html', context=dados)
