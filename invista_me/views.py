d39ba1d5c400d17f3f95c074d32a7db3c3832b8a
saulosgil <saulosgil@hotmail.com>
2024-03-18 11:17:50 -0300

    alterando e excluindo dados - CRUD

--- invista_me/views.py
+++ invista_me/views.py
@@ -3,17 +3,22 @@ from .models import Investimento
 from .forms import InvestimentoForm
 
 # Create your views here.
+
 def investimentos(request):
   dados = {
     'dados': Investimento.objects.all()
   }
   return render(request, 'investimentos/investimentos.html', context=dados)
 
+
+
 def detalhe(request, id_investimento):
   dados = {
     'dados': Investimento.objects.get(pk=id_investimento)
   }
   return render(request, 'investimentos/detalhe.html', dados)
+
+
 
 def criar(request):
   if request.method == 'POST':
@@ -27,3 +32,27 @@ def criar(request):
       'formulario': investimento_form
     }
   return render(request, 'investimentos/novo_investimento.html', context=formulario)
+
+
+
+def editar(request, id_investimento):
+  investimento = Investimento.objects.get(pk=id_investimento)
+  # novo investimento/1 -> GET
+  if request.method == 'GET':
+    formulario = InvestimentoForm(instance=investimento)
+    return render(request, 'investimentos/novo_investimento.html', {'formulario': formulario})
+  # caso requisição seja POST
+  else:
+    formulario = InvestimentoForm(request.POST, instance=investimento)
+    if formulario.is_valid():
+      formulario.save()
+    return redirect('investimentos')
+
+
+
+def excluir(request, id_investimento):
+  investimento = Investimento.objects.get(pk=id_investimento)
+  if request.method == 'POST':
+    investimento.delete()
+    return redirect('investimentos')
+  return render(request, 'investimentos/confirmar_exclusao.html', {'item':investimento})
--- projeto_invista_me/urls.py
+++ projeto_invista_me/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.investimentos, name='investimentos'),
     path('novo_investimento/', views.criar, name='novo_investimento'),
+    path('novo_investimento/<int:id_investimento>', views.editar, name='editar'),
+    path('excluir_investimento/<int:id_investimento>', views.excluir, name='excluir'),
     path('/<int:id_investimento>', views.detalhe, name='detalhe')
 ]
