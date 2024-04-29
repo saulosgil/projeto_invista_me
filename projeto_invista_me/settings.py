f9c73b0f25e3a506fb235f6b1cb0e23ef351ded4
saulosgil <saulosgil@hotmail.com>
2024-04-29 10:15:18 -0300

    git ignore

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres.zkutllayqxxmjovddkjh', 
+        'PASSWORD': 'QgrOaIDIW6rX0mGw',
+        'HOST': 'aws-0-sa-east-1.pooler.supabase.com', 
+        'PORT': '5432'
     }
 }
 
