6d7536262b8720ed658978dfdc98498e5a7c0552
saulosgil <saulosgil@hotmail.com>
2024-04-29 16:55:50 -0300

    setting

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9=d1v$i=4-!@nlqig#o@(jyzf+m^ixdbwz18^ehwx-4*g3u%cn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
