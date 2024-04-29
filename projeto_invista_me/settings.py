a48b3f2eb6a438c27e6175da0f5d5bfd2a4eb40a
saulosgil <saulosgil@hotmail.com>
2024-04-29 16:42:48 -0300

    allowed host

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9=d1v$i=4-!@nlqig#o@(jyzf+m^ixdbwz18^ehwx-4*g3u%cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
